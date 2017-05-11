6b4b31de8213e9f490cdac25a3d7b476c8cb5292
lolzcalvin <ckseong96@hotmail.com>
2017-05-11 10:10:47 +0800

    init

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+// Main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "hello world" << endl;
+    return 0;
+}
+
